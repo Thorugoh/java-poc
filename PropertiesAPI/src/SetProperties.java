359247f315ddd367eaf139f6222367c6767ca9a5
Victor Hugo <hugo.victoor77@gmail.com>
2025-09-28 07:36:19 -0300

    Set Properties

--- PropertiesAPI/src/SetProperties.java
+++ PropertiesAPI/src/SetProperties.java
@@ -1,9 +1,30 @@
+import java.io.InputStream;
 import java.util.Properties;
 
 public class SetProperties {
     public static void main(String[] args) {
-        String rootPath = Thread.currentThread().getContextClassLoader().getResource("").getPath();
-        Properties appProps = new Properties();
+        String configName = "app.properties";
+        try (InputStream inputStream = Thread.currentThread()
+                .getContextClassLoader()
+                .getResourceAsStream(configName)) {
+
+            if(inputStream == null) {
+                System.out.println("Sorry, unable to find " + configName);
+                return;
+            }
+            Properties appProps = new Properties();
+            appProps.load(inputStream);
+
+            appProps.setProperty("name", "UpdatedAppName");
+            appProps.setProperty("downloadAddr", "http://example.com/download");
+
+            System.out.println("Application Name: " + appProps.getProperty("name"));
+            System.out.println("Application Download Address: " + appProps.getProperty("downloadAddr"));
+
+
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 }
