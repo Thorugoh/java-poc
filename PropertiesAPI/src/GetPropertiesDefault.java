dee6565b4530963c01d9060ef003ca28ee6cac1e
Victor Hugo <hugo.victoor77@gmail.com>
2025-09-28 07:28:38 -0300

    fix indent

--- PropertiesAPI/src/GetPropertiesDefault.java
+++ PropertiesAPI/src/GetPropertiesDefault.java
@@ -4,22 +4,22 @@ import java.util.Properties;
 public class GetPropertiesDefault {
     public static void main(String[] args) {
         try{
-        String rootPath = Thread.currentThread().getContextClassLoader().getResource("").getPath();
-        String appConfigPath = rootPath + "app.properties";
+            String rootPath = Thread.currentThread().getContextClassLoader().getResource("").getPath();
+            String appConfigPath = rootPath + "app.properties";
 
-        Properties appProps = new Properties();
-        appProps.load(new FileInputStream(appConfigPath));
+            Properties appProps = new Properties();
+            appProps.load(new FileInputStream(appConfigPath));
 
-        String appVersion = appProps.getProperty("version");
-        String appName = appProps.getProperty("name", "defaultAppName");
-        String appGroup = appProps.getProperty("group", "defaultAppGroup");
-        String appDownloadAddr = appProps.getProperty("downloadAddr");
+            String appVersion = appProps.getProperty("version");
+            String appName = appProps.getProperty("name", "defaultAppName");
+            String appGroup = appProps.getProperty("group", "defaultAppGroup");
+            String appDownloadAddr = appProps.getProperty("downloadAddr");
 
 
-        System.out.println("Application Version: " + appVersion);
-        System.out.println("Application Name: " + appName);
-        System.out.println("Application Group: " + appGroup);
-        System.out.println("Application Download Address: " + appDownloadAddr);
+            System.out.println("Application Version: " + appVersion);
+            System.out.println("Application Name: " + appName);
+            System.out.println("Application Group: " + appGroup);
+            System.out.println("Application Download Address: " + appDownloadAddr);
         } catch (Exception e) {
             e.printStackTrace();
         }
---
+++ PropertiesAPI/src/SetProperties.java
@@ -0,0 +1,9 @@
+import java.util.Properties;
+
+public class SetProperties {
+    public static void main(String[] args) {
+        String rootPath = Thread.currentThread().getContextClassLoader().getResource("").getPath();
+        Properties appProps = new Properties();
+
+    }
+}
