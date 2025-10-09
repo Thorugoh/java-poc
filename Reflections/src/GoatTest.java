f62e4f18f9b81a8231327941b5021d91bfda3a5a
Victor Hugo <hugo.victoor77@gmail.com>
2025-10-09 18:21:47 -0300

    Constructors, methods and fields

--- Reflections/src/GoatTest.java
+++ Reflections/src/GoatTest.java
@@ -1,12 +1,27 @@
 import com.thorugoh.animals.Goat;
 import org.junit.Test;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class GoatTest{
+    private static List<String> getFieldNames(Field[] fields) {
+        List<String> fieldNames = new java.util.ArrayList<>();
+        for (Field field : fields) {
+            fieldNames.add(field.getName());
+
+        }
+        return fieldNames;
+    }
+
     @Test
     public void givenObject_whenGetsClassName_thenCorrect() {
         Object goat = new Goat("goat");
@@ -27,7 +42,7 @@ public class GoatTest{
     }
 
     @Test
-    public void givenClass_whenRecognisesModifiers_thenCorrect()throws ClassNotFoundException {
+    public void givenClass_whenRecognisesModifiers_thenCorrect() throws ClassNotFoundException {
         Class<?> goatClass = Class.forName("com.thorugoh.animals.Goat");
         Class<?> animalClass = Class.forName("com.thorugoh.animals.Animal");
 
@@ -46,5 +61,45 @@ public class GoatTest{
         Package pkg = goatClass.getPackage();
 
         assertEquals("com.thorugoh.animals", pkg.getName());
+    }
+
+    // get constructor of the goat class
+    @Test
+    public void givenClass_whenGetsConstructor_thenCorrect() throws ClassNotFoundException {
+        Class<?> goatClass = Class.forName("com.thorugoh.animals.Goat");
+
+        Constructor<?>[] constructors = goatClass.getConstructors();
+
+        assertEquals(1, constructors.length);
+        assertEquals("com.thorugoh.animals.Goat", constructors[0].getName());
+    }
+
+    // Inspect fields of the animal class
+    @Test
+    public void givenClass_whenGetsFields_thenCorrect() throws ClassNotFoundException {
+        Class<?> animalClass = Class.forName("com.thorugoh.animals.Animal");
+        Field[] fields = animalClass.getDeclaredFields();
+
+        List<String> actualFields = getFieldNames(fields);
+        assertEquals(2, actualFields.size());
+        assertTrue(actualFields.containsAll(Arrays.asList("name", "CATEGORY")));
+    }
+
+    private static List<String> getMethodNames(Method[] methods) {
+        List<String> methodNames = new ArrayList<>();
+        for (Method method : methods)
+            methodNames.add(method.getName());
+        return methodNames;
+    }
+
+    //Inspect the methods of the animal class
+    @Test
+    public void givenClass_whenGetsMethods_thenCorrect() throws ClassNotFoundException {
+        Class<?> animalClass = Class.forName("com.thorugoh.animals.Animal");
+        Method[] methods = animalClass.getDeclaredMethods();
+        List<String> actualMethods = getMethodNames(methods);
+
+        assertEquals(3, actualMethods.size());
+        assertTrue(actualMethods.containsAll(Arrays.asList("getName", "setName", "getSound")));
     }
 }
