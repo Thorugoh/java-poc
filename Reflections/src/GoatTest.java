2769c657d26a96052cbab297f03981b97c781861
Victor Hugo <hugo.victoor77@gmail.com>
2025-10-08 21:02:27 -0300

    reflections get modifiers

--- Reflections/src/GoatTest.java
+++ Reflections/src/GoatTest.java
@@ -1,7 +1,10 @@
 import com.thorugoh.animals.Goat;
 import org.junit.Test;
 
+import java.lang.reflect.Modifier;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class GoatTest{
     @Test
@@ -12,5 +15,36 @@ public class GoatTest{
         assertEquals("Goat", clazz.getSimpleName());
         assertEquals("com.thorugoh.animals.Goat", clazz.getName());
         assertEquals("com.thorugoh.animals.Goat", clazz.getCanonicalName());
+    }
+
+    @Test
+    public void givenClassName_whenCreatesObject_thenCorrect() throws ClassNotFoundException {
+        Class<?> clazz = Class.forName("com.thorugoh.animals.Goat");
+
+        assertEquals("Goat", clazz.getSimpleName());
+        assertEquals("com.thorugoh.animals.Goat", clazz.getName());
+        assertEquals("com.thorugoh.animals.Goat", clazz.getCanonicalName());
+    }
+
+    @Test
+    public void givenClass_whenRecognisesModifiers_thenCorrect()throws ClassNotFoundException {
+        Class<?> goatClass = Class.forName("com.thorugoh.animals.Goat");
+        Class<?> animalClass = Class.forName("com.thorugoh.animals.Animal");
+
+        int goatMods = goatClass.getModifiers();
+        int animalMods = animalClass.getModifiers();
+
+        assertTrue(Modifier.isPublic(goatMods));
+        assertTrue(Modifier.isPublic(animalMods));
+        assertTrue(Modifier.isAbstract(animalMods));
+    }
+
+    @Test
+    public void givenClass_whenGetsPackageInfo_thenCorrect() {
+        Goat goat = new Goat("goat");
+        Class<?> goatClass = goat.getClass();
+        Package pkg = goatClass.getPackage();
+
+        assertEquals("com.thorugoh.animals", pkg.getName());
     }
 }
