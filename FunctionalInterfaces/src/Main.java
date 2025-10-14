0df0740fca20830d0f840261886fe5a8ab954615
Victor Hugo <hugo.victoor77@gmail.com>
2025-10-14 11:01:44 -0300

    shortToByte function

--- FunctionalInterfaces/src/Main.java
+++ FunctionalInterfaces/src/Main.java
@@ -4,6 +4,15 @@ import java.util.Objects;
 import java.util.function.Function;
 
 public class Main {
+    public static byte[] transformArray(short[] array, ShortToByteFunction function) {
+        byte[] transformedArray = new byte[array.length];
+        for (int i = 0; i < array.length; i++) {
+            transformedArray[i] = function.applyAsByte(array[i]);
+        }
+
+        return transformedArray;
+    }
+
     public static void main(String[] args) {
         Map<String, Integer> nameMap = new HashMap<>();
         Integer value = nameMap.computeIfAbsent("John", String::length);
@@ -14,5 +23,14 @@ public class Main {
         Function<Integer, String> quoteIntToString = quote.compose(intToString);
 
         System.out.println("Quoted 5: " + quoteIntToString.apply(5));
+
+        short[] array ={(short) 1, (short) 2, (short) 3};
+        byte[] transformedArray = transformArray(array, s -> (byte) (s * 2));
+        System.out.print("Transformed array: ");
+        for (byte b : transformedArray) {
+            System.out.print(b + " ");
+        }
     }
+
+
 }
