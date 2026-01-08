da0420d322cd7073871337ac9d4e4969d2a6bcb3
Victor Hugo <hugo.victoor77@gmail.com>
2026-01-08 09:14:50 -0300

    add groupby with mapping

--- Streams/src/StreamCollect.java
+++ Streams/src/StreamCollect.java
@@ -2,13 +2,30 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+
 public class StreamCollect {
-    public static void main(String[] args) {
+    private static void groupByLength() {
+        System.out.println("- GroupBy Length -");
         List<String> technologies = List.of("Java", "React", "Node", "JS", "Html");
 
         Map<Integer, List<String>> groupedByLength = technologies.stream()
                 .collect(Collectors.groupingBy(String::length));
 
         System.out.println(groupedByLength);
+    }
+
+    private static void groupByLengthUppercased() {
+        System.out.println("- GroupBy Length Uppercased -");
+        List<String> technologies = List.of("Java", "React", "Node", "JS", "Html");
+
+        Map<Integer, List<String>> groupedByLength = technologies.stream()
+                .collect(Collectors.groupingBy(String::length, Collectors.mapping(String::toUpperCase, Collectors.toList())));
+
+        System.out.println(groupedByLength);
+    }
+
+    public static void main(String[] args) {
+        groupByLength();
+        groupByLengthUppercased();
     }
 }
