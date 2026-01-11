c3c2a7089393fa3a6ebdba632ac39f181a8810d9
Victor Hugo <hugo.victoor77@gmail.com>
2026-01-11 19:14:57 -0300

    add compund rule predicate

--- Streams/src/Predicates.java
+++ Streams/src/Predicates.java
@@ -1,5 +1,10 @@
+import java.util.function.Predicate;
+
 public class Predicates {
     public static void main(String[] args) {
+        Predicate<Order> isExpensive = order -> order.getPrice() > 500;
+        Predicate<Order> isCancelled = order -> order.getStatus() == Status.CANCELLED;
 
+        Predicate<Order> shouldAudit = isExpensive.and(isCancelled);
     }
 }
