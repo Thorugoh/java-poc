f6f4cdb86f6db3d189436f8d78140ab6268cab9a
Victor Hugo <hugo.victoor77@gmail.com>
2026-01-15 09:25:32 -0300

    add POC using custom Supplier method

--- StreamsSuppliers/src/UnderTheHoodPOC.java
+++ StreamsSuppliers/src/UnderTheHoodPOC.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
-
 public class UnderTheHoodPOC {
+    public static void main(String[] args) {
+        boolean systemStatus = false;
+
+        MyCustomProvider<String> manualProvider = new MyCustomProvider<String>() {
+            @Override
+            public String provide() {
+                System.out.println("-- Operation in progress --");
+                return "Data from Manual Provider";
+            }
+        };
+
+        MyCustomProvider<String> lambdaProvider = () -> {
+            System.out.println("-- Operation in progress --");
+            return "Data from Lambda Provider";
+        };
+
+        System.out.println("Starting...");
+        DataProcessor.processIfActive(systemStatus, lambdaProvider);
+        System.out.println("Finished.");
+    }
 }
