8ac861d3db00ea57995c02df6967e311d436f820
Victor Hugo <hugo.victoor77@gmail.com>
2026-02-18 08:18:12 -0300

    Simulate transfer lock

--- LockAndAtomicVariables/src/BankManager.java
+++ LockAndAtomicVariables/src/BankManager.java
@@ -1,4 +1,78 @@
-package PACKAGE_NAME;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+ class Account {
+    private double balance;
+
+    public Account(double initialBalance) {
+        this.balance = initialBalance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void withdraw(double amount) {
+        balance -= amount;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+}
 
 public class BankManager {
+    private final Lock transactionLock = new ReentrantLock();
+
+    public void transfer(Account fromAccount, Account toAccount, double amount) {
+        // We lock the entire operation
+        transactionLock.lock();
+        try {
+            if(fromAccount.getBalance() >= amount) {
+                fromAccount.withdraw(amount);
+                toAccount.deposit(amount);
+                System.out.println(Thread.currentThread().getName() + " transferred " + amount + " from Account A to Account B");
+            } else {
+                System.out.println(Thread.currentThread().getName() + " - Insufficient funds for transfer");
+            }
+        } finally {
+            transactionLock.unlock();
+        }
+    }
+
+    public static void main(String[] args) {
+        BankManager manager = new BankManager();
+        Account accountA = new Account(1000);
+        Account accountB = new Account(500);
+
+        Runnable transferTask = () -> {
+            for(int i = 0; i < 7; i++) {
+                manager.transfer(accountA, accountB, 100);
+
+                // Simulate some processing time
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        };
+
+        Thread t1 = new Thread(transferTask, "Thread-1");
+        Thread t2 = new Thread(transferTask, "Thread-2");
+
+        t1.start();
+        t2.start();
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Balance AccountA: " + accountA.getBalance());
+        System.out.println("Balance AccountB: " + accountB.getBalance());
+
+    }
 }
