74554f481fba219f5515e5dd180112cdf3dd2644
Victor Hugo <hugo.victoor77@gmail.com>
2026-02-20 09:06:03 -0300

    Overload Account

--- LockAndAtomicVariables/src/Account.java
+++ LockAndAtomicVariables/src/Account.java
@@ -1,8 +1,18 @@
 public class Account {
     private double balance;
+    private String name;
 
     public Account(double initialBalance) {
         this.balance = initialBalance;
+    }
+
+    public Account(String name, double initialBalance) {
+        this.balance = initialBalance;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public double getBalance() {
