43b2061c513afa989911de0c7c0cf099ebdedba2
Victor Hugo <hugo.victoor77@gmail.com>
2026-02-24 09:37:58 -0300

    added unique id for ordering

--- LockAndAtomicVariables/src/Account.java
+++ LockAndAtomicVariables/src/Account.java
@@ -1,14 +1,27 @@
 public class Account {
+    private final int id;
     private double balance;
     private String name;
 
     public Account(double initialBalance) {
+        this.id = -1;
         this.balance = initialBalance;
     }
 
     public Account(String name, double initialBalance) {
+        this.id = -1;
         this.balance = initialBalance;
         this.name = name;
+    }
+
+    public Account(int id, String name, double initialBalance){
+        this.id = id;
+        this.name = name;
+        this.balance = initialBalance;
+    }
+
+    public int getId(){
+        return  id;
     }
 
     public String getName() {
