89d7acb01ecca18d4bdf9379c25f244b432a30a8
Victor Hugo <hugo.victoor77@gmail.com>
2026-03-03 10:09:09 -0300

    add Person constructor

--- CustomAnnotation/src/Person.java
+++ CustomAnnotation/src/Person.java
@@ -1,5 +1,12 @@
 @JsonSerializable
 public class Person {
+
+    public Person(String firstName, String lastName, String age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = Integer.parseInt(age);
+    }
+
     @JsonElement
     private String firstName;
 
