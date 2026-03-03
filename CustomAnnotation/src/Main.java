2ca226f5037fe746f01249002901a41c389f3392
Victor Hugo <hugo.victoor77@gmail.com>
2026-03-03 10:09:28 -0300

    Convert Person to json

--- CustomAnnotation/src/Main.java
+++ CustomAnnotation/src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        try {
+            Person person = new Person("John", "Doe", "30");
+
+            ObjectToJsonConverter converter = new ObjectToJsonConverter();
+            String jsonResult = converter.convertToJson(person);
+
+            System.out.println("Generated JSON: " + jsonResult);
+
+        } catch (Exception e) {
+            System.err.println("Error during JSON serialization: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
