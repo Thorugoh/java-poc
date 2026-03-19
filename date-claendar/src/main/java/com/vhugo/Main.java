bc9dbf534ce441c1f13000df6a46dfb1ecad282c
Victor Hugo <hugo.victoor77@gmail.com>
2026-03-18 21:12:36 -0300

    Add local date example

--- date-claendar/src/main/java/com/vhugo/Main.java
+++ date-claendar/src/main/java/com/vhugo/Main.java
@@ -1,17 +1,24 @@
 package com.vhugo;
 
 
+import java.time.LocalDate;
 import java.util.Calendar;
 import java.util.Date;
 
 public class Main {
     static void main() {
+
+        // Old Calendar and Date
         Calendar cal = Calendar.getInstance();
+        // 0 indexed month
         cal.set(2024, Calendar.MARCH, 15);
         cal.add(Calendar.DAY_OF_MONTH, 30);
         Date result = cal.getTime();
 
         System.out.println(result);
         // Sun Apr 14 21:08:11 GMT-03:00 2024 // Time together
+
+        // New date time api
+        LocalDate resultLocalDate = LocalDate.of(2024, 3, 15).plusDays(30);
     }
 }
