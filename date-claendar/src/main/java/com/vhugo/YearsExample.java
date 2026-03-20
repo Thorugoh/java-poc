8d6fabe0749c79417a28c565d38901e2dcb9c010
Victor Hugo <hugo.victoor77@gmail.com>
2026-03-19 21:17:49 -0300

    add year example old vs new api

---
+++ date-claendar/src/main/java/com/vhugo/YearsExample.java
@@ -0,0 +1,21 @@
+package com.vhugo;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+public class YearsExample {
+    static void main() {
+
+        @SuppressWarnings("deprecation")
+        Date wrongDate = new Date(2024, 2, 15);
+        System.out.println("Date (wrong  year): " + wrongDate);
+
+        @SuppressWarnings("deprecation")
+        Date correctDate = new Date(124, 2, 15); // 2024 - 1900 = 124
+        System.out.println("Date (year 'correct'): " + correctDate);
+
+        // LocalDate
+        LocalDate data = LocalDate.of(2024, 3, 15);
+        System.out.println("LocalDate: " + data);
+    }
+}
