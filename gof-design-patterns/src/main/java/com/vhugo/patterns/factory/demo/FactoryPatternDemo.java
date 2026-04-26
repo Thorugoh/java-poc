d5533e35db9a17c5ad18ab3b26d3abfcb6c751b0
Victor Hugo <hugo.victoor77@gmail.com>
2026-04-26 09:53:43 -0300

    create simple factory

--- gof-design-patterns/src/main/java/com/vhugo/patterns/factory/demo/FactoryPatternDemo.java
+++ gof-design-patterns/src/main/java/com/vhugo/patterns/factory/demo/FactoryPatternDemo.java
@@ -1,4 +1,19 @@
 package com.vhugo.patterns.factory.demo;
 
+import com.vhugo.patterns.factory.model.Notification;
+import com.vhugo.patterns.factory.simple.SimpleFactoryNotification;
+
+import java.util.Optional;
+
 public class FactoryPatternDemo {
+    // simple factory demo
+    static void main() {
+        var n1 = SimpleFactoryNotification.create("sms");
+        n1.ifPresent(n -> n.send("+5512345678", "Hi Sms"));
+
+        SimpleFactoryNotification.create("fax").ifPresentOrElse(
+                n -> n.send("dest", "msg"),
+                () -> System.out.println("Unsupported type")
+        );
+    }
 }
--- gof-design-patterns/src/main/java/com/vhugo/patterns/factory/simple/SimpleFactoryNotification.java
+++ gof-design-patterns/src/main/java/com/vhugo/patterns/factory/simple/SimpleFactoryNotification.java
@@ -1,4 +1,22 @@
 package com.vhugo.patterns.factory.simple;
 
+import com.vhugo.patterns.factory.model.EmailNotification;
+import com.vhugo.patterns.factory.model.Notification;
+import com.vhugo.patterns.factory.model.PushNotification;
+import com.vhugo.patterns.factory.model.SMSNotification;
+
+import java.util.Optional;
+
 public class SimpleFactoryNotification {
+    public static Optional<Notification> create(String type) {
+        return Optional.ofNullable(
+            switch (type.toLowerCase()) {
+                case "email" -> new EmailNotification();
+                case "sms" -> new SMSNotification();
+                case "push" -> new PushNotification();
+                default -> null;
+            }
+        );
+
+    }
 }
