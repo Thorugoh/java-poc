0892ea7ba706276d04c30d21c52408d6454873b1
Victor Hugo <hugo.victoor77@gmail.com>
2026-05-04 09:40:31 -0300

    create simple email build

--- gof-design-patterns/src/main/java/com/vhugo/patterns/builder/BuilderDemo.java
+++ gof-design-patterns/src/main/java/com/vhugo/patterns/builder/BuilderDemo.java
@@ -1,4 +1,13 @@
 package com.vhugo.patterns.builder;
 
 public class BuilderDemo {
+    static void main() {
+        Email simple = new Email.Builder(
+                "maria@email.com",
+                "Meeting tomorrow",
+                "Please confirm by 6 PM.")
+                .build();
+
+        simple.display();
+    }
 }
