581c70fc77c3eb9ccc26b9ea3c0033395755a9d4
Victor Hugo <hugo.victoor77@gmail.com>
2026-06-08 09:59:00 -0300

    add setters to Rectangle

--- gof-design-patterns/src/main/java/com/vhugo/patterns/prototype/Rectangle.java
+++ gof-design-patterns/src/main/java/com/vhugo/patterns/prototype/Rectangle.java
@@ -14,8 +14,16 @@ public class Rectangle extends Shape {
         return width;
     }
 
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
     public int getHeight() {
         return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
     }
 
     @Override
